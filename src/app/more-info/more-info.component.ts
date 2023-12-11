afb1812c9c082282c4f5d0ce288581d2654631c5
NormanCao2702 <norman.cao2702@gmail.com>
2023-12-11 10:10:00 -0800

    update more-info and report container

--- src/app/more-info/more-info.component.ts
+++ src/app/more-info/more-info.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
-
+import { Component, Inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ReportService } from '../report.service';
+import { FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-more-info',
   templateUrl: './more-info.component.html',
   styleUrls: ['./more-info.component.css']
 })
-export class MoreInfoComponent {
+export class MoreInfoComponent implements OnInit{
+  report!: any;
+  selectedStatus!: string;
 
+  constructor(
+    private router: ActivatedRoute,
+    private rs: ReportService,
+    private http: HttpClient,
+    private route:Router,
+  ){
+    
+  }
+  ngOnInit(): void {
+    this.router.params.subscribe(params=>{
+      this.rs.getID(params['id']).subscribe((report)=>{
+        this.report = report;
+        console.log(this.report);
+      })
+    })
+  }
+
+  updateStatus(): void{
+    let password = prompt('Enter password for further operations:');
+
+    this.http.get<Object>('https://api.hashify.net/hash/md5/hex?value=' + password).subscribe((data: any) => {
+      password = data.Digest;
+      console.log(password);
+      if (password === 'fcab0453879a2b2281bc5073e3f5fe54') {
+        this.report.data.status = true;
+        this.rs.reportUpdated(this.report);
+      } else {
+        alert('Invalid password');
+      }
+    });
+    
+  }
+
+  back(): void{
+    this.route.navigate(['']);
+  }
 }
--- src/app/report-container/report-container.component.ts
+++ src/app/report-container/report-container.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ReportService } from '../report.service';
+import { Report } from '../report.model';
 @Component({
   selector: 'app-report-container',
   templateUrl: './report-container.component.html',
   styleUrls: ['./report-container.component.css']
 })
-export class ReportContainerComponent {
+export class ReportContainerComponent implements OnInit{
+  reports: any[]=[];
 
+  constructor(private rs: ReportService){}
+
+  ngOnInit(): void {
+    this.rs.get().subscribe((report)=>{
+      console.log("inside getting data in report container");
+      this.reports = report;
+      // console.log(this.reports[0].data.monsterInfo);
+    });
+  }
+
+  onDeleteReport(id: string){
+    this.rs.delete(id).subscribe(()=>{
+      this.reports = this.reports.filter((report)=> report.id !== id);
+    })
+  }
 }
